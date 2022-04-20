56f646bd7c599505657a92309782c7dcd54e414f
magnusblaa <magnus.moeller@hotmail.com>
2022-04-20 15:41:10 +0200

    Added Buzzer boundary class and Buzzer interface

---
+++ Microwave.Classes/Boundary/Buzzer.cs
@@ -0,0 +1,27 @@
+﻿using Microwave.Classes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microwave.Classes.Boundary
+{
+    internal class Buzzer: IBuzzer
+    {
+        private IOutput _output;
+
+        private bool IsPlaying = false;
+
+        public Buzzer(IOutput output)
+        {
+            _output = output;
+        }
+
+        public void PlaySound()
+        {
+            if(!IsPlaying)
+            {
+                _output.OutputLine("");
+            }
+        }
+    }
+}
